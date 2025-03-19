4227569123ef72a05eba922bdf711b88d50a9285
Tim Rooke <timrooke1991@gmail.com>
2025-03-19 12:38:53 +0000

    Test: Add new component in the wrong structure

---
+++ src/components/Description.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="description">
+    <p class="description__content">{{ content }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: String,
+});
+</script>
+
+<style scoped>
+.description {
+  margin: 1rem 0;
+}
+
+.description__title {
+  font-size: 1.25rem;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+  color: #1a1a1a;
+}
+
+.description__content {
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #4a4a4a;
+  display: -webkit-box;
+}
+</style>
